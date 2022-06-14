f4b165a12e856727a811eb1b4ac26ffc57b19827
masangri <masangri.art@gmail.com>
2022-06-13 18:07:34 -0700

    Create installer script for nft-blender module for Blender's Python interpreter Create instructions in README file.

--- README.md
+++ README.md
@@ -2,5 +2,28 @@
 ### **98% Tools for Blender - Not really just for NFTs...** (Web3 support on the "roadmap" 😏)
 <br/>
 
+> This package is delivered *as-is*.
+
 ## Installation
 <!-- todo -->
+### Windows
+1. Install Blender:
+   - [Microsoft Store](https://apps.microsoft.com/store/detail/blender/9PP3C07GTVRH) 
+     (recommended for users on shared machine)
+   - [MSI Installer](https://www.blender.org/download/)
+2. Install Python 3.x:
+   - [Microsoft Store](https://apps.microsoft.com/store/detail/python-39/9P7QFQMJRFP7)
+     (recommended for users on shared machine)
+   - [Application](https://www.python.org/downloads/)
+3. Install nft-blender module in Blender:
+   1. Launch Blender.
+   2. Open the System Console window (Window > Toggle System Console).
+   3. Go to the *Scripting* tab.
+   4. Copy and run the code in `scripts/install.py` from the [NFT Blender repository](https://github.com/Masangri/nft-blender).<br/>
+   5. In the System Console window, copy the Windows path in the last line of the output.
+4. Include User's Python installation directory to list of Blender's `site-packages` locations:
+   - Create PYTHONPATH environment variable for the user, if it doesn't yet exist.
+   - Set the value to the Windows path from the System Console window in step 3.5 above.
+5. Close Blender and relaunch.
+   - You will now be able to run `import nft_blender` to access scripts.
+   - Note that there is no custom menu add-on yet, and scripts will need to be run from the *Scripting* tab by the user.
--- scripts/install.py
+++ scripts/install.py
@@ -8,6 +8,10 @@ import subprocess
 import sys
 
 
+__LOGGER__ = logging.getLogger(__name__)
+__LOGGER__.setLevel(logging.INFO)
+
+
 MODULES = {
     'nft_blender': 'git+https://github.com/Masangri/nft-blender.git@main#egg=nft-blender',
     # Dependencies (installed automatically with `nft-blender` module):
@@ -18,46 +22,55 @@ MODULES = {
 
 def _ensure_pip():
     """Ensure that `pip` is installed (comes preinstalled for Blender 3.x)."""
-    logging.debug('Checking if pip is installed:\n')
+    __LOGGER__.debug('Checking if pip is installed:\n')
     subprocess.call([sys.executable, '-m', 'ensurepip'])
 
 
-def install_package(module_name: str):
+def _get_user_site() -> pathlib.Path:
+    """"""
+    return pathlib.Path(site.getusersitepackages())
+
+
+def install_package(module_name: str) -> bool:
     """"""
     # Retrieve module name and location
     module_location = MODULES.get(module_name)
 
     # Abort if module info isn't found.
     if not module_location:
-        logging.warning(f'`{module_name}` is not a valid option. Aborting!')
+        __LOGGER__.warning(f'`{module_name}` is not a valid option. Aborting!')
         return False
 
     # Upgrade `pip` if necessary.
-    logging.debug('Updating pip')
+    __LOGGER__.debug('Updating pip')
     subprocess.call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
 
     # Access to Blender's internal site packages might be restricted on Windows.
     # Install package to user's Python site-packages directory instead (same version of Python 3.x recommended).
-    user_site = pathlib.Path(site.getusersitepackages())
+    user_site = _get_user_site()
     if str(user_site) not in site.getsitepackages():
-        logging.debug('Adding user\'s `site-packages` directory as potential installation location.')
+        __LOGGER__.debug('Adding user\'s `site-packages` directory as potential installation location.')
         site.addsitedir(str(user_site))
-
 
     # Install module and dependencies from GitHub repository (branch: `main`), if not already.
     installed_module_names = [mod.name for mod in pkgutil.iter_modules()]
     if module_name not in installed_module_names:
-        logging.debug(f'Installing Python module: `{module_name}`')
+        __LOGGER__.debug(f'Installing Python module: `{module_name}`')
         subprocess.call([sys.executable, '-m', 'pip', 'install', module_location])
 
     # Update module.
-    logging.debug(f'Updating Python module: `{module_name}`')
+    __LOGGER__.debug(f'Updating Python module: `{module_name}`')
     subprocess.call([sys.executable, '-m', 'pip', 'install', module_location, '--upgrade'])
+
+    __LOGGER__.info(
+        '\n\nPlease create the following user environment variable:\n'
+        f'PYTHONPATH="{str(user_site)}"\n'
+    )
 
     return True
 
 
-def uninstall_package(module_name: str):
+def uninstall_package(module_name: str) -> bool:
     """"""
     # Uninstall module and dependencies, if they are installed.
     installed_module_names = [mod.name for mod in pkgutil.iter_modules()]
@@ -65,7 +78,9 @@ def uninstall_package(module_name: str):
         logging.debug(f'Uninstalling Python module: `{module_name}`')
         subprocess.call([sys.executable, '-m', 'pip', 'uninstall', '-y', module_name])
 
-    return True
+        return True
+
+    return False
 
 
 if __name__ == '__main__':
