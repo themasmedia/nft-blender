57041c684fc289f08c409765d9604fbe8086da90
masangri <masangri@DESKTOP-RLBPRK8.localdomain>
2022-07-21 11:32:39 -0700

    Update nft_sql.py Add logger. Add various utility functions and error checking.

--- src/nft_blender/nft_db/nft_sql.py
+++ src/nft_blender/nft_db/nft_sql.py
@@ -1,14 +1,23 @@
 #!$BLENDER_PATH/python/bin python
 
+import logging
 import pathlib
+import sys
+import typing
 
 import sqlalchemy
+import sqlalchemy.exc
+import sqlalchemy.ext.declarative
 import sqlalchemy.orm
 import sqlalchemy.orm.exc
-import sqlalchemy.ext.declarative
 
 
-class DB_ObjectBase(object):
+__LOGGER__ = logging.getLogger(__name__)
+__LOGGER__.addHandler(logging.StreamHandler(sys.stdout))
+__LOGGER__.setLevel(logging.INFO)
+
+
+class DBObjectBase(object):
     """TODO"""
     def __getitem__(self, field):
         """TODO"""
@@ -26,18 +35,18 @@ class DB_ObjectBase(object):
     id =  sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
 
 
-DB_ObjectBase = sqlalchemy.ext.declarative.declarative_base(cls=DB_ObjectBase)
+DBObjectBase = sqlalchemy.ext.declarative.declarative_base(cls=DBObjectBase)
 
 
-DB_UserProjects = sqlalchemy.Table(
+DBUserProjects = sqlalchemy.Table(
     'user_projects',
-    DB_ObjectBase.metadata,
+    DBObjectBase.metadata,
     sqlalchemy.Column('project_id', sqlalchemy.ForeignKey('projects.id'), primary_key=True),
     sqlalchemy.Column('user_id', sqlalchemy.ForeignKey('users.id'), primary_key=True),
 )
 
 
-class DB_Project(DB_ObjectBase):
+class DBProject(DBObjectBase):
     """TODO"""
     __tablename__ = 'projects'
     code = sqlalchemy.Column(sqlalchemy.String, unique=True)
@@ -45,62 +54,89 @@ class DB_Project(DB_ObjectBase):
     path = sqlalchemy.Column(sqlalchemy.String)
     pipeline = sqlalchemy.Column(sqlalchemy.JSON)
     users = sqlalchemy.orm.relationship(
-        'DB_User',
-        order_by='DB_User.id',
-        secondary=DB_UserProjects,
+        'DBUser',
+        order_by='DBUser.id',
+        secondary=DBUserProjects,
         back_populates='projects',
     )
 
 
-class DB_User(DB_ObjectBase):
+class DBUser(DBObjectBase):
     """TODO"""
     __tablename__ = 'users'
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
     projects = sqlalchemy.orm.relationship(
-        'DB_Project',
-        order_by='DB_Project.id',
-        secondary=DB_UserProjects,
+        'DBProject',
+        order_by='DBProject.id',
+        secondary=DBUserProjects,
         back_populates='users',
     )
 
 
-def db_create_table(
-    db_engine: sqlalchemy.engine.base.Engine,
-    drop_existing: bool = False
-):
-    """TODO"""
-    if drop_existing:
-        DB_ObjectBase.metadata.drop_all(db_engine)
-
-    DB_ObjectBase.metadata.create_all(db_engine)
-
-
-def db_get_columns(db_row: DB_ObjectBase) -> dict:
+def db_get_columns(db_row: DBObjectBase) -> dict:
     """TODO"""
     return db_row.__class__.__table__.columns
 
 
-def db_get_engine(db_url: pathlib.Path | str):
+def db_create_table(
+    db_engine: sqlalchemy.engine.base.Engine,
+    drop_existing: bool = False,
+):
     """TODO"""
-    return sqlalchemy.create_engine(db_url, echo=True)
+    if drop_existing:
+        DBObjectBase.metadata.drop_all(db_engine)
+
+    DBObjectBase.metadata.create_all(db_engine)
+
+
+def db_get_engine(
+    db_url: pathlib.Path | str,
+) -> sqlalchemy.engine.base.Engine | None:
+    """TODO"""
+    try:
+        return sqlalchemy.create_engine(db_url, echo=True)
+    except sqlalchemy.exc.ArgumentError:
+        return None
 
 
 def db_get_url(
     db_name: str,
     root_dir_path: pathlib.Path | str,
-) -> pathlib.Path:
+    dbms_name: str = 'SQLite',
+) -> str:
     """SQLite only"""
     root_dir_path = pathlib.Path(root_dir_path)
     db_file_path = root_dir_path.joinpath(db_name).with_suffix('.db')
-    db_url = f'sqlite:///{db_file_path.as_posix()}'
+    db_url = ''
+
+    if dbms_name == 'SQLite':
+        db_url = f'sqlite:///{db_file_path.as_posix()}'
 
     return db_url
 
 
+def db_test_connection(
+    db_engine: sqlalchemy.engine.base.Engine = None
+) -> bool:
+    """TODO"""
+    try:
+        assert isinstance(db_engine, sqlalchemy.engine.base.Engine)
+        db_engine.connect()
+        __LOGGER__.info('Database connection verified.')
+
+        return True
+
+    except (AssertionError, sqlalchemy.exc.SQLAlchemyError) as sqle:
+        logger_msg = f'Database error: {sqle}.'
+        __LOGGER__.warning(logger_msg)
+
+        return False
+
+
 def db_upsert(
     db_engine: sqlalchemy.engine.base.Engine,
-    column_name_filter: str,
-    *db_entries: tuple[DB_ObjectBase],
+    db_entries: typing.Iterable[DBObjectBase] = (),
+    column_name_filter: str = None,
 ) -> bool | tuple[bool]:
     """TODO"""
     db_entries_updated = []
@@ -110,38 +146,46 @@ def db_upsert(
         for db_entry in db_entries:
 
             try:
+                db_cls = db_entry.__class__
+                # Query the DB using a simple equal operator for the given column attribute.
+                if column_name_filter:
+                    db_col_attr = db_cls.__table__.columns[column_name_filter]
+                    db_query = db_session.query(db_cls) \
+                                         .filter(db_col_attr == db_entry[column_name_filter])
 
-                db_col_attr = db_entry.__class__.__table__.columns[column_name_filter]
-
-                db_query = db_session.query(db_entry.__class__) \
-                                     .filter(db_col_attr == db_entry[column_name_filter])
-
-                if db_query.one_or_none() is None:
-
-                    db_session.add(db_entry)
-
+                # Otherwise default to querying using the Primary Key column, aka "id".
                 else:
+                    db_query = db_session.query(db_cls).get(db_entry.id)
 
-                    for k, v in db_get_columns(db_entry).items():
-                        if not v.primary_key:
-                            db_query.update({k: db_entry[k]})
+                # If no results are found, add the entry to the DB
+                if db_query.one_or_none() is None:
+                    db_session.add(db_entry)
+                    logger_msg = f'{db_cls} entry added for {db_entry}.'
+                    __LOGGER__.debug(logger_msg)
 
+                # Otherwise, update all columns for the existing entry.
+                else:
+                    for key, val in db_get_columns(db_entry).items():
+                        if not val.primary_key:
+                            db_query.update({key: db_entry[key]})
+                    logger_msg = f'{db_cls} entry updated for {db_entry}.'
+                    __LOGGER__.debug(logger_msg)
 
             except sqlalchemy.orm.exc.MultipleResultsFound as sqle:
-
-                print(f'Database error: {sqle}.')
-                print('Two or more tables found with the same name: {db_entry.name}.')
+                logger_msg = f'Database error: {sqle}. \
+                               Two or more tables found with the same name: {db_entry.name}.'
+                __LOGGER__.warning(logger_msg)
                 db_entries_updated.append(False)
 
-            except Exception as e:
-                print(f'Database error: {e}.')
+            except sqlalchemy.exc.SQLAlchemyError as sqle:
+                logger_msg = f'Database error: {sqle}.'
+                __LOGGER__.warning(logger_msg)
                 db_entries_updated.append(False)
 
             finally:
-
                 db_session.flush()
                 db_entries_updated.append(True)
 
         db_session.commit()
 
-    return tuple(db_entries_updated) if len(db_entries) > 1 else db_entries_updated[0]
+    return tuple(db_entries_updated)
