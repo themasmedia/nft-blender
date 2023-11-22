684aebf9ae51f763ceabb5e45ff0e8f045f359cd
mas <masangri.art@gmail.com>
2023-11-21 18:31:58 -0800

    Fix bug in bpy_node to include node groups in return value.

--- src/nft_blender/nft_bpy/bpy_node.py
+++ src/nft_blender/nft_bpy/bpy_node.py
@@ -12,14 +12,18 @@ import bpy
 
 def node_get_nodes_from_node_tree(
     node_tree,
-    sub_grps: bool = True,
     node_types: typing.Iterable = (bpy.types.Node),
+    sub_grps: bool = True,
 ) -> list:
     """Works for any tree of nodes, including Materials, Groups, and NodeTrees"""
 
     nodes = []
 
     for n in node_tree.nodes:
+
+        if isinstance(n, node_types):
+            nodes.append(n)
+
         if sub_grps and isinstance(n, (
             bpy.types.CompositorNodeGroup,
             bpy.types.GeometryNodeGroup,
@@ -27,8 +31,10 @@ def node_get_nodes_from_node_tree(
             bpy.types.ShaderNodeGroup,
             bpy.types.TextureNodeGroup,
         )):
-            nodes.extend(node_get_nodes_from_node_tree(n.node_tree, sub_grps, node_types))
-        elif isinstance(n, node_types):
-            nodes.append(n)
+            nodes.extend(node_get_nodes_from_node_tree(
+                n.node_tree,
+                sub_grps,
+                node_types
+            ))
     
     return nodes
