e3fb1784498ff172eb6ff93404c1dfb762bcf3dc
mas <masangri.art@gmail.com>
2023-12-02 15:25:42 -0800

    Create nft_py sub-module for Python helper functions.

---
+++ src/nft_blender/nft_py/__init__.py
@@ -0,0 +1,11 @@
+#!$BLENDER_PATH/python/bin python
+
+"""
+NFT Blender - PY (Blender Python)
+
+.. code-block:: python
+
+    # Import NFT Blender - PY
+    from nft_blender.nft_py import py_util
+
+"""
