d1d717072af2e7765ed3ab23b1cbd8594ed7a170
mas <masangri.art@gmail.com>
2024-05-02 22:02:04 -0700

    Develop shapefile import script in ops_v3d. Create py_paths utility module for path utils.

--- src/nft_blender/nft_ops/ops_v3d.py
+++ src/nft_blender/nft_ops/ops_v3d.py
@@ -12,7 +12,7 @@ import typing
 
 import bpy
 
-from nft_blender.nft_bpy._bpy_core import bpy_scn
+from nft_blender.nft_bpy._bpy_core import bpy_ctx, bpy_scn
 from nft_blender.nft_py import py_util
 
 
@@ -42,3 +42,46 @@ def v3d_add_custom_props(
             mtl_prop_data = copy.copy(V3D_CONFIG_DATA['material'])
             for mtl in obj_mtls:
                 bpy_scn.scn_edit_custom_props(mtl, mtl_prop_data, remove_extra, update_existing)
+
+
+def v3d_import_shapefile(
+    shapefile_path = pathlib.Path | str,
+    elevation_source: str = 'GEOM',
+    extrusion_axis: str = 'Z',
+    field_elevaction_name: str = '',
+    field_extrude_name: str = '',
+    field_object_name: str = '',
+    object_elevation_name: str = '',
+    scaler: float = 1.0,
+    separate_objects: bool = False,
+    shape_crs: str = ''
+) -> bpy.types.Object:
+    """"""
+    shapefile_path = pathlib.Path(shapefile_path)
+
+    bgis_prefs = bpy_ctx.ctx_get_addon('BlenderGIS-228').preferences
+    predef_crs_json = json.loads(bgis_prefs['predefCrsJson'])[0]
+    shp_crs = shape_crs or predef_crs_json[bgis_prefs['predefCrs']]
+
+    bpy.ops.importgis.shapefile(
+        'INVOKE_DEFAULT',
+        filepath=shapefile_path.as_posix(),
+        elevSource=elevation_source,
+        extrusionAxis=extrusion_axis,
+        fieldElevName=field_elevaction_name,
+        fieldExtrudeName=field_extrude_name,
+        fieldObjName=field_object_name,
+        objElevName=object_elevation_name,
+        separateObjects=separate_objects,
+        shpCRS=shp_crs,
+    )
+
+    shp_obj = bpy.context.active_object
+
+    bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
+    shp_obj.scale *= scaler
+    bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)
+
+    bpy_scn.scn_select_items()
+
+    return shp_obj
---
+++ src/nft_blender/nft_py/py_paths.py
@@ -0,0 +1,49 @@
+#!$BLENDER_PATH/python/bin python
+
+"""
+NFT Blender - PY -  PATH
+
+"""
+
+import pathlib
+import typing
+
+
+def paths_get_contents(
+    dir_path: str | pathlib.Path,
+    dirs: bool = True,
+    files: bool = True,
+    recursive: bool = False,
+    suffix_filter: typing.Iterable[str] = None
+) -> list:
+    """
+    Returns a list of Path objects for directories and/or files in the given directory.
+
+    Args:
+        dir_path (Path): The directory to search.
+        dirs (bool, optional): If True, includes directory paths in the output list. Defaults to True.
+        files (bool, optional): If True, includes file paths in the output list. Defaults to True.
+        recursive (bool, optional): If True, includes all subdirectories recursively. Defaults to False.
+        suffix_filter (Tuple[str], optional): A tuple of file suffixes to filter files by. Defaults to None.
+
+    Returns:
+        list: A list of Path objects for the requested directories and/or files.
+    """
+    dir_path = pathlib.Path(dir_path)
+    paths = []
+
+    if recursive:
+        for path in dir_path.rglob('*'):
+            if path.is_dir() and dirs:
+                paths.append(path)
+            elif path.is_file() and files and (suffix_filter is None or path.suffix in suffix_filter):
+                paths.append(path)
+
+    else:
+        for path in dir_path.iterdir():
+            if path.is_dir() and dirs:
+                paths.append(path)
+            elif path.is_file() and files and (suffix_filter is None or path.suffix in suffix_filter):
+                paths.append(path)
+
+    return paths
